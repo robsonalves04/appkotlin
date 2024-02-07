a4784fa07bc8e3f684e3115e9f39354a4caca070
Robson Alves <106483036+robsonalves04@users.noreply.github.com>
2024-02-07 16:54:03 -0300

    💩cleaning and working well

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -3,9 +3,3 @@ package com.example.portfolioapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
